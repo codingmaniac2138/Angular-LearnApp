dd53a2049743b30b9f1704b26659fd94f962ac54
suhasjadhav23 <suhasjadhav23@gmail.com>
2018-02-13 12:54:31 +0530

    learning done till angualar templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  months = ["January", "Feburary", "March", "April", "May", 
+            "June", "July", "August", "September",
+            "October", "November", "December"];
+  isavailable = true;   //variable is set to true
+  myClickFunction(event) { 
+      //just added console.log which will display the event details in browser on click of the button.
+      alert("Button is clicked");
+      console.log(event);
+   }
+
+  changemonths(event) {
+      alert("Changed month from the Dropdown");
+      console.log("Changed month from the Dropdown");
+      console.log(event);
+   }
+
 }
