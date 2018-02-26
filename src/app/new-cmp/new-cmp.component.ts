380e0866b9f0477498ffd175f4872a1f1e536ef6
suhasjadhav23 <suhasjadhav23@gmail.com>
2018-02-26 14:36:41 +0530

    final commit of the day

--- src/app/new-cmp/new-cmp.component.ts
+++ src/app/new-cmp/new-cmp.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCmpComponent implements OnInit {
   newcomponent = "Entered in new component created";
-
+  
 
   constructor() { }
 
