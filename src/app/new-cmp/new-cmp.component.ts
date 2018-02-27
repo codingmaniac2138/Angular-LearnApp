bad5d32cfe78f03ea900afad2d988397c26e9736
suhasjadhav23 <suhasjadhav23@gmail.com>
2018-02-27 12:18:51 +0530

    click function initialize for dynamica row creations and deletion

--- src/app/new-cmp/new-cmp.component.ts
+++ src/app/new-cmp/new-cmp.component.ts
@@ -8,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class NewCmpComponent implements OnInit {
   newcomponent = "Entered in new component created";
   
+  insertrow(event) { 
+    //just added console.log which will display the event details in browser on click of the button.
+    alert("Button is clicked");
+    var table = document.getElementById("tbl");
 
+var tr = document.createElement("tr");
+var td = document.createElement("td");
+var txt = document.createTextNode("some value");
+
+td.appendChild(txt);
+tr.appendChild(td);
+table.appendChild(tr);
+  }
   constructor() { }
 
   ngOnInit() {
